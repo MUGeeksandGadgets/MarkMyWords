1a6ba8274062cd2070d0d54da3c92dbcf34cff97
Patrick Shinn <shinn5112@gmail.com>
2017-10-07 14:56:26 -0400

    tile set updated, scene 1 updated

--- data/Raw Map Files/Tiles.tsx
+++ data/Raw Map Files/Tiles.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Open Jam 2017" tilewidth="8" tileheight="8" tilecount="90" columns="15">
+<tileset name="Tiles" tilewidth="8" tileheight="8" tilecount="90" columns="15">
  <image source="tiles1.png" width="120" height="48"/>
 </tileset>
